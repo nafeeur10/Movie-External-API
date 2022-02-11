75685e6ee053934802974bd0bea7370420826bed
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2022-02-12 01:05:05 +0600

    env file added. axios library added. get Genres List from our given API

---
+++ src/components/Genres.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li v-for="genre in genresList" :key="genre.id">
+            {{ genre.name }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'Genres',
+  data() {
+      return {
+        genresList: []
+      }
+  }, 
+  methods: {
+      getGenresList() {
+          this.$http.get(process.env.VUE_APP_BASE_MOVIE_URL + 'genre/movie/list',{
+              params: { api_key: process.env.VUE_APP_API_KEY}
+          }).then((response) => {
+              this.genresList = response.data.genres
+          })
+      }
+  }, 
+  mounted() {
+      this.getGenresList()
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
